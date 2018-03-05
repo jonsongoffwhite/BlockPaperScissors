6c729ae2645ab067c6c05279d40a443b5f61dc88
jhg14 <jonnygoffwhite@gmail.com>
2018-03-05 14:14:27 +0000

    Remove default code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var RockPaperScissors = artifacts.require("./RockPaperScissors.sol")
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(RockPaperScissors);
 };
